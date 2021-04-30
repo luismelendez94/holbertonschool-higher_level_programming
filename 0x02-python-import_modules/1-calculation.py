9b326ee85a7136198d998deebc6663ef65966359
luismelendez94 <2709@holbertonschool.com>
2021-04-30 01:15:17 +0000

    Calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+add = __import__("calculator_1").add
+sub = __import__("calculator_1").sub
+mul = __import__("calculator_1").mul
+div = __import__("calculator_1").div
+
+
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, sub(a, b)))
+print("{} + {} = {}".format(a, b, mul(a, b)))
+print("{} + {} = {}".format(a, b, div(a, b)))
