ce3941fd61aa1412a44042ea8e2be8a20766cf75
luismelendez94 <2709@holbertonschool.com>
2021-04-30 16:25:37 +0000

    Adding if main

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-add = __import__("calculator_1").add
-sub = __import__("calculator_1").sub
-mul = __import__("calculator_1").mul
-div = __import__("calculator_1").div
 
+if __name__ == "__main__":
+    add = __import__("calculator_1").add
+    sub = __import__("calculator_1").sub
+    mul = __import__("calculator_1").mul
+    div = __import__("calculator_1").div
 
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} + {} = {}".format(a, b, sub(a, b)))
-print("{} + {} = {}".format(a, b, mul(a, b)))
-print("{} + {} = {}".format(a, b, div(a, b)))
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
