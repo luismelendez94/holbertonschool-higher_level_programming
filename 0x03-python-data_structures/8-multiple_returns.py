1080523a0c7d4317469c835ac72d31f3826c5e2f
luismelendez94 <2709@holbertonschool.com>
2021-05-05 23:51:13 +0000

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return (None)
+    new_tuple = (0, '')
+    new_tuple = (len(sentence), sentence[0])
+    return (new_tuple)
