ed8d37138f58d02f9e501d2492a79cdfda811ff2
luismelendez94 <2709@holbertonschool.com>
2021-05-05 03:04:04 +0000

    No C allowed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    result_string = ""
+    for i in range(0, len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            result_string = result_string + my_string[i]
+    return (result_string)
