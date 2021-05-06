3c642cd450c467ea4facf2b23abcec289afb73e6
luismelendez94 <2709@holbertonschool.com>
2021-05-06 00:03:54 +0000

    Find the Max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    maxNumber = 0
+    for item in my_list:
+        if item > maxNumber:
+            maxNumber = item
+    return maxNumber
