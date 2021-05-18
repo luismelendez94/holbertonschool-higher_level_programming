4982ef290fab6dcf75cfb57cff1d8e9abb8cb33d
luismelendez94 <2709@holbertonschool.com>
2021-05-18 18:54:24 +0000

    Adding Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""This is the class Square"""
+
+
 class Square:
+    """Adding size to the class"""
+
     def __init__(self, new_size):
+        """Initialize variable size"""
         self.__size = new_size
