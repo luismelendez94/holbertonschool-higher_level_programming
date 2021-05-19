7a8c0eeb65b5efcca4f81f4471e13bf4645c7c45
luismelendez94 <2709@holbertonschool.com>
2021-05-19 02:56:01 +0000

    changing to if

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,9 +8,8 @@ class Square:
     def __init__(self, size=0):
         """Initialize variable size Verify that it is a integer"""
 
-        try:
-            self.__size = int(size)
-            if size < 0:
-                raise ValueError("size must be >= 0")
-        except TypeError:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
