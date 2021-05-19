8f1fa024d1d6eecd856fe94ace832cd682d38802
luismelendez94 <2709@holbertonschool.com>
2021-05-19 02:22:07 +0000

    Adding int verify

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,11 +6,10 @@ class Square:
     """Size validation of the square"""
 
     def __init__(self, size=0):
-        """Initialize variable size
-        Verify that it is a integer"""
+        """Initialize variable size Verify that it is a integer"""
 
         try:
-            self.__size = size
+            self.__size = int(size)
             if size < 0:
                 raise ValueError("size must be >= 0")
         except TypeError:
