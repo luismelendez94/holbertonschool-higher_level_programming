f521bbc20907c83c432ee11a98d46b27b3918a06
luismelendez94 <2709@holbertonschool.com>
2021-05-25 01:44:43 +0000

    Editing pep8

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -70,4 +70,5 @@ class Rectangle:
     def __repr__(self):
         """Returns a string representation of the rectangle"""
 
-        return "Rectangle(" + str(self.__width) + ", " + str(self.__height) + ")"
+        return "Rectangle(" + str(self.__width) + ", \
+" + str(self.__height) + ")"
