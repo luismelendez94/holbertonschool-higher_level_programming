ad6fe560bfe0fd5f1c43006a1fb75abcf28076ca
luismelendez94 <2709@holbertonschool.com>
2021-06-03 03:03:09 +0000

    Changing condition

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,6 @@
 def is_same_class(obj, a_class):
     """ Verify if the object its from class """
 
-    if isinstance(obj, a_class):
+    if (type(obj) == a_class):
         return True
     return False
