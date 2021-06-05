8354539ed2d43ea22e436cc081175be35fe7add3
luismelendez94 <2709@holbertonschool.com>
2021-06-05 04:57:01 +0000

    Editing if

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,6 @@
 def inherits_from(obj, a_class):
     """ Verify if obj is from class """
 
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
