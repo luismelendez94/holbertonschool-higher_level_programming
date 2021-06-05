f705bb232afd6f9638dc8a27a6c7d544acf5c2af
luismelendez94 <2709@holbertonschool.com>
2021-06-05 03:14:11 +0000

    pep editing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ Script to add all argument to a python list and save them to a file """
 
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-import sys
 
 fileName = "add_item.json"
 
