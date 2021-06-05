46af378353493875591dba681706f70a0afc0a99
luismelendez94 <2709@holbertonschool.com>
2021-06-05 03:51:22 +0000

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This is the class Student """
+
+
+class Student:
+    """ Class that defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialize the instances in Student """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves the dictionary representation """
+
+        if attrs != None:
+            newList = {}
+            for index in attrs:
+                if index in self.__dict__:
+                    newList[index] = self.__dict__[index]
+            return newList
+
+        return(self.__dict__)
