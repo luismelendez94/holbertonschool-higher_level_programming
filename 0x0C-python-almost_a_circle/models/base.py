fedf65b6011e30653f6103493335c80b968d63ce
luismelendez94 <2709@holbertonschool.com>
2021-06-09 03:56:31 +0000

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -64,7 +64,7 @@ class Base():
 
     @classmethod
     def load_from_file(cls):
-        """  """
+        """ Returns a list of instances """
 
         filename = cls.__name__ + ".json"
         returnList = []
