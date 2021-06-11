feebfce8086b1c1c43b38d5ace00a1730793aa0e
luismelendez94 <2709@holbertonschool.com>
2021-06-11 04:05:43 +0000

    Unique Addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+
+
 def uniq_add(my_list=[]):
 
+    new_list = []
+    for element in my_list:
+        if element not in new_list:
+            new_list.append(element)
+    addition = 0
+    for element in new_list:
+        addition += element
+    return addition
