19278321d6dd1cb45fe4e2020646e6fe3584558b
luismelendez94 <2709@holbertonschool.com>
2021-06-11 04:34:49 +0000

    Only Different

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,8 +6,8 @@ def only_diff_elements(set_1, set_2):
     new_list = []
     for element1 in set_1:
         for element2 in set_2:
-            if element2 not in set_1 and element2 not in new_list:
-                new_list.append(element2)
-            elif element1 not in set_2 and element1 not in new_list:
+            if element1 not in set_2 and element1 not in new_list:
                 new_list.append(element1)
+            elif element2 not in set_1 and element2 not in new_list:
+                new_list.append(element2)
     return new_list
