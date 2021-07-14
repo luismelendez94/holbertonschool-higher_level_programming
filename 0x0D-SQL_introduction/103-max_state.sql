ae1a2b04dd5d270439bad4da9872bb913da2656e
luismelendez94 <2709@holbertonschool.com>
2021-07-14 03:32:59 +0000

    Task 20

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- Task 20: Temperatures #2
+-- Display the max temperature of each state ordered by State
+SELECT state, MAX(value) AS 'max_temp'
+FROM temperatures
+GROUP BY state
+ORDER BY state;
