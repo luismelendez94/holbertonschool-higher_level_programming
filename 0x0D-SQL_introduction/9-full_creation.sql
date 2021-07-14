d3b47656966273b4e25d4286ec9c1fade9c257de
luismelendez94 <2709@holbertonschool.com>
2021-07-14 01:18:21 +0000

    Task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,4 @@
+-- Task 9: Full creation
+-- Create a table in your MySQL server
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
