0d90a84f444102967013d291696aedd0bcfa644c
luismelendez94 <2709@holbertonschool.com>
2021-07-14 23:50:38 +0000

    Task 0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Task 0: My privileges!
+-- List all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
