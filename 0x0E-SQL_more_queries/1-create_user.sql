8cf2412a290543b659e70a3b5723d3f9bfb80302
luismelendez94 <2709@holbertonschool.com>
2021-07-15 00:38:11 +0000

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Task 1: Root user
 -- Create the MySQL server user user_0d_1
-DROP USER 'user_0d_1'@'localhost';
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON * . * TO 'user_0d_1'@'localhost';
