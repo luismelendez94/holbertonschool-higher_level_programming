cc6968096c14c380eede94f0994b78b0fca18485
luismelendez94 <2709@holbertonschool.com>
2021-07-16 03:22:18 +0000

    Task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Task 16: List shows and genres
+-- List all shows, and all genres linked to that show, from hbtn_0d_tvshows database
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
