b37b52aa56f8e30fc94cfc2a83dd56ac0a71efc8
luismelendez94 <2709@holbertonschool.com>
2021-08-03 02:29:04 +0000

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const process = require('process');
 const args = process.argv;
 
 if (args.length < 3) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (args.length === 3) {
   console.log('Argument found');
 } else {
