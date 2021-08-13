a7f5b2c49362b1da6f074a9e19029536e7b352b8
luismelendez94 <2709@holbertonschool.com>
2021-08-13 02:59:02 +0000

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    sql = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC;"
 
     try:
         cursor.execute(sql)
