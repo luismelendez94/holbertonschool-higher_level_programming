fa3d2e6d61532b521c2752ecc7db485532607116
luismelendez94 <2709@holbertonschool.com>
2021-08-13 04:44:38 +0000

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,12 +8,12 @@ of that state, using the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server = "localhost"
     user = sys.argv[1]
     passw = sys.argv[2]
     dbname = sys.argv[3]
-    stateName = sys.argv[4].split(" ")
+    stateName = sys.argv[4].split(' ')
 
     db = MySQLdb.connect(server, user, passw, dbname)
 
@@ -22,17 +22,17 @@ if __name__ == '__main__':
     sql = "SELECT cities.id, cities.name, states.name FROM cities\
             INNER JOIN states ON cities.state_id = states.id\
             WHERE states.name LIKE BINARY '{}'\
-            ORDER BY cities.id ASC;".format(stateName[0])
+            ORDER BY cities.id ASC".format(stateName[0])
     try:
         cursor.execute(sql)
         results = cursor.fetchall()
-        citiesNames = ''
+        citiesNames = ""
         for row in results:
-            citiesName += row[1]
-            citiesName += ", "
-        print(citiesName[:-2])
-    except:
-        print("Cant connect")
+            citiesNames += row[1]
+            citiesNames += ", "
+        print(citiesNames[:-2])
+    except Exception as e:
+        print(e)
         db.rollback
 
     db.close()
