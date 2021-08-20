1d57389f4826d9d5d2f2f1d85fc2327451b32168
luismelendez94 <2709@holbertonschool.com>
2021-08-20 02:37:10 +0000

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Takes an URL, sends a request to that URL and display the size of the body of the response
-curl -s $1 | grep -i Content-Length | awk '{print $2}'
+curl -s $1 --include | grep -i Content-Length | awk '{print $2}'
