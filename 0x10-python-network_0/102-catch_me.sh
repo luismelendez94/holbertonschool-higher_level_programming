131b3660df9400bc28577f36eec5c2ce1cdcd58c
luismelendez94 <2709@holbertonschool.com>
2021-09-18 02:49:58 +0000

    Task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send JSON POST to an URL passed as the first argument, displays the body of the response
-curl -s -L -d "You got me!" -X 'PUT' "0.0.0.0:5000/catch_me"
+curl -sL -X PUT -d "You got me!" -H "0.0.0.0:5000/catch_me"
