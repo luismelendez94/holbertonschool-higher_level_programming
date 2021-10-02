75bd25ff9c2a601f2e2a5aa4acaf0ad5b4a62d1d
luismelendez94 <2709@holbertonschool.com>
2021-10-02 02:03:15 +0000

    Task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL,
+sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    print(requests.get(url).headers.get("X-Request-Id"))
